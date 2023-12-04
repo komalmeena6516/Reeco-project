d8b6efa790c547032f8165c9adf951eecbe1b1f3
Komal Meena <21meenakk@gmail.com>
2023-12-04 17:12:52 +0530

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import "./banner.css";
 
 const Banner = () => {
   return (
-    <div>
-        <div className="data"style={{margin: '0 100px'}}>
-            <p>Order <ChevronRightRoundedIcon/> <span style={{textTransform: 'underline'}}>Order3245ABC</span></p>
+    <div className='banner'>
+        <div className="data" style={{margin: '0 100px'}}>
+            <p>Order <ChevronRightRoundedIcon className='rightarw'/> <span className='underline'>Order3245ABC</span></p>
+
+            
             <div className="order" style={{display: 'flex', justifyContent: 'space-between'}}>
                 <p className="orderNum">Order 32457ABC</p>
                 <div className="buttons">
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -30,10 +30,10 @@ const style = {
     borderRadius: '6px',
     boxShadow: 24,
     p: 4,
-    pl:5,
-    pr:5,
+    pl: 5,
+    pr: 5,
     border: 'none !important'
-  };
+};
 
 
 
@@ -49,33 +49,33 @@ const Tablecomp = () => {
     const [approved, setApproved] = useState(false);
     const [Missing, setMissing] = useState(false);
     const [urgentMissing, setUrgentMissing] = useState(false);
-    
+
 
     //approve function
-        const handleApprove = (index) => {
-             setApproved(!approved);
-             console.log(approved);
-            // Create a copy of the data array
-            const newData = [...data];
-            // Toggle the approved state of the clicked item
-            newData[index].approved = !newData[index].approved;
-            // Update the state with the new data
-            newData[index].Status = newData[index].approved ? 'Approved' : '';
+    const handleApprove = (index) => {
+        setApproved(!approved);
+        console.log(approved);
+        // Create a copy of the data array
+        const newData = [...data];
+        // Toggle the approved state of the clicked item
+        newData[index].approved = !newData[index].approved;
+        // Update the state with the new data
+        newData[index].Status = newData[index].approved ? 'Approved' : '';
 
-            setData(newData);
-         };
+        setData(newData);
+    };
 
-         //missing-urgent
-         const handleUrgentMissing = (index)=>{
-            setApproved(!approved);
-            setUrgentMissing(!urgentMissing);
-            const newData = [...data];
-newData[index].urgentMissing = !newData[index].urgentMissing;
+    //missing-urgent
+    const handleUrgentMissing = (index) => {
+        setApproved(!approved);
+        setUrgentMissing(!urgentMissing);
+        const newData = [...data];
+        newData[index].urgentMissing = !newData[index].urgentMissing;
 
-            newData[index].Status = newData[index].urgentMissing ? 'Missing Urgent': '';
+        newData[index].Status = newData[index].urgentMissing ? 'Missing Urgent' : '';
 
-         }
-    
+    }
+
 
     useEffect(() => {
         axios.get('http://localhost:3000/products').then((res) => {
@@ -87,93 +87,93 @@ newData[index].urgentMissing = !newData[index].urgentMissing;
 
     return (
         <>
-     
-        <div className='table-box'>
-          
 
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="a dense table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>img</TableCell>
-                            <TableCell>Product Name</TableCell>
-                            <TableCell>Brand</TableCell>
-                            <TableCell>Price</TableCell>
-                            <TableCell>Quantity</TableCell>
-                            <TableCell>Total</TableCell>
-                            <TableCell>Status</TableCell>
-                            <TableCell>last</TableCell>
+            <div className='table-box'>
 
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {data.map((d, ind) => (
-                            <TableRow
-                                key={d.ind}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                            <img style={{width: '50px'}} src={photo} alt="" />
-                                </TableCell>
-                                <TableCell align="left">{d.Product_name}</TableCell>
-                                <TableCell align="left">{d.Brand}</TableCell>
-                                <TableCell align="left">${d.Price}/6+1LB</TableCell>
-                                <TableCell align="left">{d.Quantity}*6+1LB</TableCell>
-                                <TableCell align="left">{d.Total}</TableCell>
-                                <TableCell align="left" className='status'>
-                                {/* {d.Status} */}
+
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="a dense table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell></TableCell>
+                                <TableCell>Product Name</TableCell>
+                                <TableCell>Brand</TableCell>
+                                <TableCell>Price</TableCell>
+                                <TableCell>Quantity</TableCell>
+                                <TableCell>Total</TableCell>
+                                <TableCell>Status</TableCell>
+                                <TableCell></TableCell>
+
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {data.map((d, ind) => (
+                                <TableRow
+                                    key={d.ind}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        <img style={{ width: '50px' }} src={photo} alt="" />
+                                    </TableCell>
+                                    <TableCell align="left">{d.Product_name}</TableCell>
+                                    <TableCell align="left">{d.Brand}</TableCell>
+                                    <TableCell align="left">${d.Price}/6+1LB</TableCell>
+                                    <TableCell align="left">{d.Quantity}*6+1LB</TableCell>
+                                    <TableCell align="left">{d.Total}</TableCell>
+                                    <TableCell align="left" className='status'>
+                                        {/* {d.Status} */}
                                         {d.approved && (
-                                            <span className='statuskagreentext' style={{ color: 'white',  borderRadius: "20px",  }}>Approved</span>
+                                            <span className='statuskagreentext' style={{ color: 'white', borderRadius: "20px", }}>Approved</span>
                                         )}
                                         {d.urgentMissing && (
-<span className='statusUrgenttext'>Missing Urgent</span>
+                                            <span className='statusUrgenttext'>Missing Urgent</span>
                                         )}
-                                </TableCell>
-                                <TableCell align="left" className='last-col'>
-                                <CheckRoundedIcon
+                                    </TableCell>
+                                    <TableCell align="left" className='last-col'>
+                                        <CheckRoundedIcon
                                             onClick={() => handleApprove(ind)}
                                             className={d.approved ? 'greentick' : ''}
                                         />
-                                    <CloseRoundedIcon onClick={()=>handleOpen(ind)}
-                                    className={d.urgentMissing ? "redcross" : '' }
-                                    />
-                                    <span> Edit</span>
-                                   </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </div>
-
-        {/* modal--------- */}
-        <div>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-            <div className="modal-head">
-                <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ fontWeight: 500 }}>
-          Missing Product
-          </Typography>
-         <CloseRoundedIcon onClick={handleClose} sx={{color: grey}}/>
+                                        <CloseRoundedIcon onClick={() => handleOpen(ind)}
+                                            className={d.urgentMissing ? "redcross" : ''}
+                                        />
+                                        <span> Edit</span>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
-          
-          <Typography id="modal-modal-description" sx={{ mt: 1 }}>
-        Is 'Chicken Breast Fillets,Boneless...' urgent?
-          </Typography>
-          <Typography sx={{mt: 4, display:'flex', justifyContent: 'flex-end', gap:4, }}>
-          <button className='no'>No</button>
-          <button onClick={()=>handleUrgentMissing()} className='yes'>Yes</button>
-          </Typography>
 
-        </Box>
-      </Modal>
-    </div>
+            {/* modal--------- */}
+            <div>
+                <Button onClick={handleOpen}>Open modal</Button>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <div className="modal-head">
+                            <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ fontWeight: 500 }}>
+                                Missing Product
+                            </Typography>
+                            <CloseRoundedIcon onClick={handleClose} sx={{ color: grey }} />
+                        </div>
+
+                        <Typography id="modal-modal-description" sx={{ mt: 1 }}>
+                            Is 'Chicken Breast Fillets,Boneless...' urgent?
+                        </Typography>
+                        <Typography sx={{ mt: 4, display: 'flex', justifyContent: 'flex-end', gap: 4, }}>
+                            <button className='no'>No</button>
+                            <button onClick={() => handleUrgentMissing()} className='yes'>Yes</button>
+                        </Typography>
+
+                    </Box>
+                </Modal>
+            </div>
 
         </>
     )
