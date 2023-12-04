859c0077292b788baa185ff05f1e6362bffa1221
Komal Meena <21meenakk@gmail.com>
2023-12-04 14:31:58 +0530

    testing

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
 
 const Banner = () => {
   return (
     <div>
-        <div className="data">
-            <p>Order <span>Order3245ABC</span></p>
-            <div className="order">
+        <div className="data"style={{margin: '0 100px'}}>
+            <p>Order <ChevronRightRoundedIcon/> <span style={{textTransform: 'underline'}}>Order3245ABC</span></p>
+            <div className="order" style={{display: 'flex', justifyContent: 'space-between'}}>
                 <p className="orderNum">Order 32457ABC</p>
+                <div className="buttons">
+                    <button className="back">Back</button>
+                    <button className="approve-order">Approve order</button>
+                </div>
             </div>
         </div>
     </div>
